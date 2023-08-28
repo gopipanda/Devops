c9a0bfb2cc609cc9988afbadd57d9b889c2be37f
gopipanda <gopicg10@gmail.com>
2023-08-28 13:33:14 +0530

    java commit

---
+++ exp1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class exp1
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Name:Pinniboina Rajesh");
+		System.out.println("Tkno:NEC0821049");
+		System.out.print("\nAge:17");
+		System.out.printf("\nPhno:9989548717");
+	}
+}
